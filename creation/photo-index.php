9622cb928e8084bd4a9036fe001eb20ad7e9a1d1
Unknown <alan@xmlteam.com>
2018-09-28 16:16:38 -0700

    position

--- creation/photo-index.php
+++ creation/photo-index.php
@@ -11,7 +11,6 @@
 <body>
 	<div class="photoQuestion">
 		<h1><span class="photoKicker">Take a photo of somebody...</span> wearing black and white</h1>
-</h3>
 	</div>
   <div id="container">
     <div id="vid_container">
@@ -19,6 +18,9 @@
         <div id="video_overlay"></div>
     </div>
     <div id="gui_controls">
+	<div class="photoQuestion">
+		<h1><span class="photoKicker">2Take a photo of somebody...</span> wearing black and white</h1>
+	</div>
         <button id="switchCameraButton" name="switch Camera" type="button" aria-pressed="false"></button>
         <button id="takePhotoButton" name="take Photo" type="button"></button>
         <button id="toggleFullScreenButton" name="toggle FullScreen" type="button" aria-pressed="false"></button>
