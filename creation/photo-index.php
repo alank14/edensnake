18c315f3fbce21e6d27513ef33834c1ef19e57f4
Unknown <alan@xmlteam.com>
2018-09-28 16:22:54 -0700

    pix

--- creation/photo-index.php
+++ creation/photo-index.php
@@ -9,18 +9,15 @@
   <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	<div class="photoQuestion">
-		<h1><span class="photoKicker">Take a photo of somebody...</span> wearing black and white</h1>
-	</div>
   <div id="container">
     <div id="vid_container">
         <video id="video" autoplay playsinline></video>
         <div id="video_overlay"></div>
     </div>
     <div id="gui_controls">
-	<div class="photoQuestion">
-		<h1><span class="photoKicker">2Take a photo of somebody...</span> wearing black and white</h1>
-	</div>
+		<div class="photoQuestion">
+			<p>someone<br/>wearing black<br/>and white</p>
+		</div>
         <button id="switchCameraButton" name="switch Camera" type="button" aria-pressed="false"></button>
         <button id="takePhotoButton" name="take Photo" type="button"></button>
         <button id="toggleFullScreenButton" name="toggle FullScreen" type="button" aria-pressed="false"></button>
